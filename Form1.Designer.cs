0475696f6c84acafda2029a89943ce95997d5936
diego <136401880+DKB-17@users.noreply.github.com>
2024-11-04 12:06:12 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Trabalho_STL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 19);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(161, 47);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Numero de pontas";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.numericUpDown2);
+            this.groupBox2.Location = new System.Drawing.Point(181, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(173, 47);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Raio interno";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(6, 19);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 0;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.numericUpDown3);
+            this.groupBox3.Location = new System.Drawing.Point(14, 72);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(161, 47);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Altura";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(6, 19);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 0;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.numericUpDown4);
+            this.groupBox4.Location = new System.Drawing.Point(181, 72);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(173, 47);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Raio Externo";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(6, 19);
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 0;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(360, 41);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Gerar STL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.groupBox1);
+            this.groupBox5.Controls.Add(this.groupBox2);
+            this.groupBox5.Controls.Add(this.groupBox4);
+            this.groupBox5.Controls.Add(this.groupBox3);
+            this.groupBox5.Location = new System.Drawing.Point(12, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(360, 140);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Dados";
+            this.groupBox5.Enter += new System.EventHandler(this.groupBox5_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 211);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,246 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalho_STL
+{
+    public partial class Form1 : Form
+    {
+        public class Triangulo
+        {
+            public Vector3d a { get;}
+            public Vector3d b { get;}
+            public Vector3d c { get;}
+
+            public Triangulo(Vector3d a, Vector3d b, Vector3d c) 
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+        }
+
+        public class Face
+        {
+            public List<Triangulo> face { get; }
+
+            public Face()
+            {
+                face = new List<Triangulo>();
+            }
+        }
+
+        public class Estrela
+        {
+            public List<Face> faces { get; set; }
+            public Estrela() 
+            { 
+                faces = new List<Face>();
+            }
+        }
+
+        int numeroDePontas;
+        int altura;
+        int raioInterno;
+        int raioExterno;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox5_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numeroDePontas = (int)numericUpDown1.Value;
+            altura = (int)numericUpDown3.Value;
+            raioInterno = (int)numericUpDown2.Value;
+            raioExterno = (int)numericUpDown4.Value;
+
+            Estrela star = new Estrela();
+            Face f = new Face();
+            Triangulo t;
+            float ge = ((360 / numeroDePontas)/2);
+            // face inferior
+            for (float gi = 0; ge <= 360; gi += (360 / numeroDePontas), ge += (360 / numeroDePontas))
+            {
+                double degInRad_gi = gi * 3.1415 / 180;
+                double cos_gi = Math.Cos(degInRad_gi);
+                double sin_gi = Math.Sin(degInRad_gi);
+                double degInRad_ge = ge * 3.1415 / 180;
+                double cos_ge = Math.Cos(degInRad_ge);
+                double sin_ge = Math.Sin(degInRad_ge);
+                double xb = cos_gi * raioInterno;
+                double yb = sin_gi * raioInterno;
+                double xc = cos_ge * raioExterno;
+                double yc = sin_ge * raioExterno;
+
+                t = new Triangulo(
+                    new Vector3d(0, 0, 0),
+                    new Vector3d(xb, yb, 0),
+                    new Vector3d(xc, yc, 0)
+                    );
+                f.face.Add(t);
+
+                float gip = gi + (360 / numeroDePontas);
+                double degInRad_gip = gip * 3.1415 / 180;
+                double cos_gip = Math.Cos(degInRad_gip);
+                double sin_gip = Math.Sin(degInRad_gip);
+                xb = cos_gip * raioInterno;
+                yb = sin_gip * raioInterno;
+                t = new Triangulo(
+                    new Vector3d(0, 0, 0),
+                    new Vector3d(xb, yb, 0),
+                    new Vector3d(xc, yc, 0)
+                    );
+               
+                f.face.Add(t);
+            }
+            star.faces.Add(f);
+            f = new Face();
+
+            ge = ((360 / numeroDePontas) / 2);
+            // face superior
+            for (float gi = 0; ge <= 360; gi += (360 / numeroDePontas), ge += (360 / numeroDePontas))
+            {
+                double degInRad_gi = gi * 3.1415 / 180;
+                double cos_gi = Math.Cos(degInRad_gi);
+                double sin_gi = Math.Sin(degInRad_gi);
+                double degInRad_ge = ge * 3.1415 / 180;
+                double cos_ge = Math.Cos(degInRad_ge);
+                double sin_ge = Math.Sin(degInRad_ge);
+                double xb = cos_gi * raioInterno;
+                double yb = sin_gi * raioInterno;
+                double xc = cos_ge * raioExterno;
+                double yc = sin_ge * raioExterno;
+
+                t = new Triangulo(
+                    new Vector3d(0, 0, altura),
+                    new Vector3d(xb, yb, altura),
+                    new Vector3d(xc, yc, altura)
+                    );
+                f.face.Add(t);
+
+                float gip = gi + (360 / numeroDePontas);
+                double degInRad_gip = gip * 3.1415 / 180;
+                double cos_gip = Math.Cos(degInRad_gip);
+                double sin_gip = Math.Sin(degInRad_gip);
+                xb = cos_gip * raioInterno;
+                yb = sin_gip * raioInterno;
+                t = new Triangulo(
+                    new Vector3d(0, 0, altura),
+                    new Vector3d(xb, yb, altura),
+                    new Vector3d(xc, yc, altura)
+                    );
+
+                f.face.Add(t);
+            }
+
+            star.faces.Add(f);
+            f = new Face();
+
+            ge = ((360 / numeroDePontas) / 2);
+            // face externa
+            for (float gi = 0; ge <= 360; gi += (360 / numeroDePontas), ge += (360 / numeroDePontas))
+            {
+                double degInRad_gi = gi * 3.1415 / 180;
+                double cos_gi = Math.Cos(degInRad_gi);
+                double sin_gi = Math.Sin(degInRad_gi);
+                double degInRad_ge = ge * 3.1415 / 180;
+                double cos_ge = Math.Cos(degInRad_ge);
+                double sin_ge = Math.Sin(degInRad_ge);
+                double xb = cos_gi * raioInterno;
+                double yb = sin_gi * raioInterno;
+                double xc = cos_ge * raioExterno;
+                double yc = sin_ge * raioExterno;
+
+                t = new Triangulo(
+                    new Vector3d(xb, yb, 0),
+                    new Vector3d(xb, yb, altura),
+                    new Vector3d(xc, yc, 0)
+                    );
+                f.face.Add(t);
+                t = new Triangulo(
+                    new Vector3d(xb, yb, altura),
+                    new Vector3d(xc, yc, altura),
+                    new Vector3d(xc, yc, 0)
+                    );
+                f.face.Add(t);
+
+                float gip = gi + (360 / numeroDePontas);
+                double degInRad_gip = gip * 3.1415 / 180;
+                double cos_gip = Math.Cos(degInRad_gip);
+                double sin_gip = Math.Sin(degInRad_gip);
+                xb = cos_gip * raioInterno;
+                yb = sin_gip * raioInterno;
+
+                t = new Triangulo(
+                    new Vector3d(xb, yb, 0),
+                    new Vector3d(xb, yb, altura),
+                    new Vector3d(xc, yc, 0)
+                    );
+
+                f.face.Add(t);
+                t = new Triangulo(
+                    new Vector3d(xb, yb, altura),
+                    new Vector3d(xc, yc, altura),
+                    new Vector3d(xc, yc, 0)
+                    );
+                f.face.Add(t);
+            }
+
+            star.faces.Add(f);
+
+            criarArquivo(star);
+        }
+
+        public Vector3d getNormal(Vector3d a, Vector3d b, Vector3d c)
+        {
+            return Vector3d.Cross(a - c, b - c).Normalized();
+        }
+
+        public void criarArquivo(Estrela star)
+        {
+            if (star == null) return;
+            Vector3d normal = new Vector3d();
+            StreamWriter arquivo = new StreamWriter("estrela.stl");
+            arquivo.WriteLine("solid estrela");
+            foreach (Face f in star.faces)
+            {
+                foreach (Triangulo t in f.face)
+                {
+                    normal = getNormal(t.a, t.b, t.c);
+                    arquivo.WriteLine("facet normal {0} {1} {2}", normal[0].ToString(CultureInfo.InvariantCulture.NumberFormat), normal[1].ToString(CultureInfo.InvariantCulture.NumberFormat), normal[2].ToString(CultureInfo.InvariantCulture.NumberFormat));
+                    arquivo.WriteLine("     outer loop");
+                    arquivo.WriteLine("         vertex {0} {1} {2}", t.a[0].ToString(CultureInfo.InvariantCulture.NumberFormat), t.a[1].ToString(CultureInfo.InvariantCulture.NumberFormat), t.a[2].ToString(CultureInfo.InvariantCulture.NumberFormat));
+                    arquivo.WriteLine("         vertex {0} {1} {2}", t.b[0].ToString(CultureInfo.InvariantCulture.NumberFormat), t.b[1].ToString(CultureInfo.InvariantCulture.NumberFormat), t.b[2].ToString(CultureInfo.InvariantCulture.NumberFormat));
+                    arquivo.WriteLine("         vertex {0} {1} {2}", t.c[0].ToString(CultureInfo.InvariantCulture.NumberFormat), t.c[1].ToString(CultureInfo.InvariantCulture.NumberFormat), t.c[2].ToString(CultureInfo.InvariantCulture.NumberFormat));
+                    arquivo.WriteLine("     endloop");
+                    arquivo.WriteLine("endfacet");
+
+                }
+            }
+            arquivo.WriteLine("endsolid estrela");
+            arquivo.Close();
+            MessageBox.Show("Arquivo STL gerado.");
+        }
+    }
+}
